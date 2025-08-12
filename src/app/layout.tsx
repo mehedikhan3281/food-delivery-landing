acffc6324688044e9e0dc15a9050166fcdbb7ede
Md. Mehedi Hasan Khan <mehedikhan3281@gmail.com>
2025-08-12 21:06:42 +0600

    added scroll to top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import ResponsiveNavbar from "@/components/Shared/Navbar/ResponsiveNavbar";
 import Provider from "@/components/HOC/Provider";
 import Footer from "@/components/Shared/Footer/Footer";
+import ScrollToTop from "@/components/HOC/ScrollToTop";
 
 const font = Manrope({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
@@ -29,6 +30,7 @@ export default function RootLayout({
           <ResponsiveNavbar />
           {children}
           <Footer/>
+          <ScrollToTop/>
         </Provider>
       </body>
     </html>
